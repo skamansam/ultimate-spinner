cfacc029fa36664a1996f818e9e0952971df9519
Samuel C. Tyler <skamansam@gmail.com>
2025-01-19 17:50:57 -0500

    start serious work on the spinner

---
+++ src/lib/Spinner.svelte
@@ -0,0 +1,38 @@
+<script>
+	export let items = ["item One", "item Two", "item Three"];
+	export let spinDuration = 3000;
+	export let title = "New Spinner!";
+</script>
+
+{#if title}
+	<div style="grid-column: 1 / -1; text-align: center;">{title}</div>
+{/if}
+<ul class="spinner" style="--item-count: ${items.length}; --spin-duration: ${spinDuration}ms;">
+	{#each items as item, idx}
+		<li class="item" style="--idx: ${idx};">{item}</li>
+	{/each}
+</ul>
+
+<style>
+	ul.spinner {
+		all: unset;
+		aspect-ratio: 1 / 1;
+		background: crimson;
+		container-type: inline-size;
+		direction: ltr;
+		display: grid;
+		place-content: center start;
+	}
+	ul.spinner li {
+		align-content: center;
+		background: deepskyblue;
+		display: grid;
+		font-size: 5cqi;
+		grid-area: 1 / -1;
+		list-style: none;
+		padding-left: 1ch;
+		transform-origin: center right;
+		width: 50cqi;
+		rotate: calc(360deg / var(--item-count) * calc(var(--idx) - 1));
+	}
+</style>
