211a82c7e8374013b4eb24956b25c22d8a1cd093
Samuel C. Tyler <skamansam@gmail.com>
2025-01-20 11:46:19 -0500

    add determination of winning item

--- src/lib/Spinner.svelte
+++ src/lib/Spinner.svelte
@@ -40,72 +40,92 @@
 		);
 
 		previousEndDegree = newEndDegree;
+
+		const normalizeAngle = (finalAngle) => {
+			return (360 - finalAngle + 90) % 360;
+		};
+
+		const segment = 360 / items.length;
+		const offset = 15;
+
+		animation.onfinish = () => {
+			const finalAngle = newEndDegree % 360;
+			const normalizedAngle = normalizeAngle(finalAngle);
+			const winner = Math.floor(((normalizedAngle + offset) % 360) / segment);
+			console.log(items[winner]);
+		};
 	}
 </script>
 
-{#if title}
-	<div class="text-gray-800 dark:text-gray-200" style="grid-column: 1 / -1; text-align: center;">
-		{title}
-	</div>
-{/if}
-<ul
-	class="spinner"
-	style="--item-count: {items.length}; --spin-duration: {spinDuration}ms;"
-	on:click={spin}
-	on:keydown={(e) => e.key === 'Enter' && spin()}
-	tabindex="0"
-	role="button"
-	aria-label="Spin the wheel"
-	bind:this={spinner}
->
-	{#each items as item, idx}
-		<li class="item" style="--idx: {idx};">{item}</li>
-	{/each}
-</ul>
+<div class="spinner">
+	{#if title}
+		<div class="text-gray-800 dark:text-gray-200" style="grid-column: 1 / -1; text-align: center;">
+			{title}
+		</div>
+	{/if}
+	<ul
+		style="--item-count: {items.length}; --spin-duration: {spinDuration}ms;"
+		on:click={spin}
+		on:keydown={(e) => e.key === 'Enter' && spin()}
+		tabindex="0"
+		role="button"
+		aria-label="Spin the wheel"
+		bind:this={spinner}
+	>
+		{#each items as item, idx}
+			<li class="item" style="--idx: {idx};">{item}</li>
+		{/each}
+	</ul>
+</div>
 
 <style>
-	ul.spinner {
+	.spinner {
 		all: unset;
-		box-sizing: border-box;
-		font-family: system-ui, sans-serif;
 		aspect-ratio: 1 / 1;
-		background: var(--spinner-bg, rgb(147, 51, 234)); /* purple-600 */
 		container-type: inline-size;
 		direction: ltr;
 		display: grid;
-		place-content: center start;
-		clip-path: inset(0 0 0 0 round 50%);
-	}
-	ul.spinner li {
-		/* align-content: center;
-		background: var(--spinner-item-bg, rgb(216, 180, 254));
-		display: grid;
-		font-size: 5cqi;
-		grid-area: 1 / -1;
-		list-style: none;
-		padding-left: 1ch;
-		transform-origin: center right;
-		width: 50cqi;
-		rotate: calc(360deg / var(--item-count) * calc(var(--idx) - 1));
-		color: var(--spinner-text-color, rgb(88, 28, 135));
-		background: hsl(calc(360deg / var(--item-count) * calc(var(--idx) - 1)), 100%, 75%);
-		height: calc((2 * pi * 50cqi) / var(--item-count));
-		clip-path: polygon(0% -2%, 100% 50%, 0% 102%);
-		aspect-ratio: 1 / calc(2 * tan(180deg / var(--item-count))); */
-		align-content: center;
-		background: hsl(calc(360deg / var(--item-count) * calc(var(--idx))), 100%, 75%);
-		clip-path: polygon(0% 0, 100% 50%, 0% 100%);
-		display: grid;
-		font-size: 5cqi;
-		grid-area: 1 / -1;
-		aspect-ratio: 1 / calc(2 * tan(180deg / var(--item-count)));
-		list-style: none;
-		padding-left: 1ch;
-		rotate: calc(360deg / var(--item-count) * calc(var(--idx) - 1));
-		transform-origin: center right;
-		width: 50cqi;
-	}
+		position: relative;
+		width: 100%;
+		&::after {
+			aspect-ratio: 1 / cos(30deg);
+			background-color: var(--spinner-arrow-color, crimson);
+			clip-path: polygon(50% 100%, 100% 0, 0 0);
+			content: '';
+			height: 4cqi;
+			position: absolute;
+			place-self: start center;
+			translate: 0px 6cqi;
+			scale: 1.4;
+		}
+		ul {
+			all: unset;
+			box-sizing: border-box;
+			font-family: system-ui, sans-serif;
+			aspect-ratio: 1 / 1;
+			background: var(--spinner-bg, rgb(147, 51, 234)); /* purple-600 */
+			container-type: inline-size;
+			direction: ltr;
+			display: grid;
+			place-content: center start;
+			clip-path: inset(0 0 0 0 round 50%);
 
+			& li {
+				align-content: center;
+				background: hsl(calc(360deg / var(--item-count) * calc(var(--idx))), 100%, 75%);
+				clip-path: polygon(0% 0, 100% 50%, 0% 100%);
+				display: grid;
+				font-size: 5cqi;
+				grid-area: 1 / -1;
+				aspect-ratio: 1 / calc(2 * tan(180deg / var(--item-count)));
+				list-style: none;
+				padding-left: 1ch;
+				rotate: calc(360deg / var(--item-count) * calc(var(--idx) - 1));
+				transform-origin: center right;
+				width: 50cqi;
+			}
+		}
+	}
 	:global(.dark) ul.spinner {
 		--spinner-bg: rgb(126, 34, 206); /* purple-700 */
 		--spinner-item-bg: rgb(168, 85, 247); /* purple-500 */
